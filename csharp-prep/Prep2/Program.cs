2acd7b10ea7df8a48588bc40f1dc9acae9f443e3
pac21003@byui.edu <pac21003@byui.edu>
2023-04-23 00:46:59 -0500

    One likes to work and one doesn't

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade for the class?");
+        string grade = Console.ReadLine();
+        int percent = int.Parse(grade);
+        
+        string letter = "";
+
+        if (grade >= 90)
+            {
+                letter = "A";
+            }
+        else if (grade >= 80)
+            {
+                letter = "B";
+            }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter} ");
+        if (grade >= 70)
+            {
+                Console.WriteLine("You Passed!");
+            }
+        else
+            {
+                Console.WriteLine("Better luck next time!");
+            }
     }
 }
