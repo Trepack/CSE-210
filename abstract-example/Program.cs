56b64b52ab1f3993dd83f0513d82c8954194067c
pac21003@byui.edu <pac21003@byui.edu>
2023-04-26 23:40:49 -0500

    the code be jank always and forever

---
+++ abstract-example/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Book mistborn = new Book("Mistborn", "Brandon Sanderson");
+//Book lightningthief = new Book("Lightning thief", "Rick Riordan");
+
+
+mistborn.Display();
+//lightningthief.Display();
+
+Console.WriteLine(mistborn.IsAvaliable());
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("what is the magic number? ");
+        int magicnumber = int.Parse (Console.ReadLine());
+        Console.WriteLine("what is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        
+
+        while (guess != magicnumber)
+        {
+            
+            if (guess > magicnumber)
+            {
+                Console.WriteLine("lower");
+            }
+            else if (guess < magicnumber)
+            {
+                Console.WriteLine("higher");
+            }
+            else 
+            {
+                Console.WriteLine("you guessed right!");
+            }
+        }
     }
 }
